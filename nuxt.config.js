f748ed81b7e2e55b747dca45f5d464fe50778c68
George <goonsta@gmail.com>
2023-01-17 16:54:49 +1000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@nuxtjs/style-resources' //for globally defined style resources
+    '@nuxtjs/style-resources',
   ],
   styleResources: {
     scss: [
