af69ffd63315d100b43fbe16f8da3d2d9c57f033
George <goonsta@gmail.com>
2023-01-17 17:01:25 +1000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,6 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/scss/theme-common.scss'
@@ -30,7 +29,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@nuxtjs/style-resources',
+    '@nuxtjs/style-resources'
   ],
   styleResources: {
     scss: [
